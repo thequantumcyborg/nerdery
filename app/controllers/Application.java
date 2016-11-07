664c689801f671dfd9f1753c5bdb4d5597c95517
unknown <seifu001@umn.edu>
2016-11-06 23:44:03 -0600

    [FIX] Services > Suggestion Page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,8 +13,13 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
+    
+    public static void home(){
+        render("@Application.home");
+    }
+ 
 
-    public static void login(String username, String password) throws IOException {
+    public static void suggestion(String username) throws IOException {
         List snacklistAPI = Snack.getsnackList();
         ArrayList snacklist = new ArrayList();
        
@@ -25,6 +30,18 @@ public class Application extends Controller {
        
         //on v.2 send export refernce of snack name from snack object instead
         render("@Application.suggest", snacklist);
+    }
+    
+    public static void post_suggestion_selection(String sid){
+        boolean done=false;
+        
+      
+    }
+    
+    public static void post_suggestion_box(String name,String purchaseloc){
+        boolean done=false;
+        
+      
     }
 
 }
