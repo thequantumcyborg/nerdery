43644913b1f75f256a0fffb5e2f4047769966399
unknown <seifu001@umn.edu>
2016-11-07 04:59:11 -0600

    [FEATURE] ENABLE JPA

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,38 +10,62 @@ import models.*;
 
 public class Application extends Controller {
 
+    //emp db exists, use this just for test
+
+    private static String username = "seifu.kirubel64@gmail.com";
+    private static String eid = "ks001";
+
     public static void index() {
+        
         render();
     }
-    
-    public static void home(){
-        render("@Application.home");
-    }
- 
 
-    public static void suggestion(String username) throws IOException {
+    public static void home() {
+        render("@Application.home", username);
+    }
+
+    public static void suggestion(String user) throws IOException {
+        username = user;
         List snacklistAPI = Snack.getsnackList();
         ArrayList snacklist = new ArrayList();
-       
+
         // on v.2 use enahanced for loop 
         for (int i = 0; i < snacklistAPI.size(); i++) {
-            snacklist.add((Snack)snacklistAPI.get(i));
+            snacklist.add((Snack) snacklistAPI.get(i));
         }
-       
+
         //on v.2 send export refernce of snack name from snack object instead
-        render("@Application.suggest", snacklist);
+        render("@Application.suggest", snacklist, username);
     }
-    
-    public static void post_suggestion_selection(String sid){
-        boolean done=false;
-        
-      
+
+    public static void post_suggestion_selection(String sid, String user) throws IOException {
+
+        boolean done = false;
+        List snacklistAPI = Snack.getsnackList();
+        ArrayList snacklist = new ArrayList();
+
+        // on v.2 use enahanced for loop 
+        for (int i = 0; i < snacklistAPI.size(); i++) {
+            snacklist.add((Snack) snacklistAPI.get(i));
+        }
+
+        done = Suggest.postSelection(sid, eid);
+        if (done) {
+            String success = "Selection Submitted";
+            //on v.2 send export refernce of snack name from snack object instead
+            render("@Application.home", snacklist, success, username);
+        } else {
+            //on v2 use flash
+            String error = "Selection failed please try again.";
+            //on v.2 send export refernce of snack name from snack object instead
+            render("@Application.suggest", snacklist, error, username);
+        }
+
     }
-    
-    public static void post_suggestion_box(String name,String purchaseloc){
-        boolean done=false;
-        
-      
+
+    public static void post_suggestion_box(String name, String purchaseloc) {
+        boolean done = false;
+
     }
 
 }
--- app/models/Suggest.java
+++ app/models/Suggest.java
@@ -5,15 +5,115 @@
  */
 package models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Table;
 import play.data.validation.Required;
+import play.db.jpa.JPA;
 
 /**
  *
  * @author User
  */
+@Entity
+@Table(name = "suggest")
 public class Suggest {
-    @Required private String suggest_id;
-    @Required private String eid;
-    @Required private String sid;
-    @Required private String stat_id;
+    @Id
+    private String suggest_id;
+    @Column(name = "eid")
+    private String eid;
+    @Column(name = "snackid")
+    private String sid;
+    @Column(name = "stat_id")
+    private String stat_id;
+
+    public Suggest() {
+    }
+   
+    public Suggest(String suggest_id, String eid, String sid, String stat_id) {
+        this.suggest_id = suggest_id;
+        this.eid = eid;
+        this.sid = sid;
+        this.stat_id = stat_id;
+    }
+
+    public String getSuggest_id() {
+        return suggest_id;
+    }
+
+    public void setSuggest_id(String suggest_id) {
+        this.suggest_id = suggest_id;
+    }
+
+    public String getEid() {
+        return eid;
+    }
+
+    public void setEid(String eid) {
+        this.eid = eid;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getStat_id() {
+        return stat_id;
+    }
+
+    public void setStat_id(String stat_id) {
+        this.stat_id = stat_id;
+    }
+
+    public static boolean postSelection(String sid, String eid) {
+       
+        
+        boolean status=false;
+        
+      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "SnaFooAPI_JPA" );
+      
+      EntityManager entitymanager = emfactory.createEntityManager( );
+      entitymanager.getTransaction( ).begin( );
+
+      Suggest employee = new Suggest(); 
+     
+      employee.setSuggest_id("s001");
+      employee.setEid("e001");
+      employee.setSid("snack1001");
+      employee.setStat_id("stat110");
+      entitymanager.persist( employee );
+      entitymanager.getTransaction( ).commit( );
+
+      entitymanager.close( );
+      emfactory.close( );
+ 
+       
+        status=true;
+        
+        
+        
+        return status;
+       
+
+    }
+
 }
